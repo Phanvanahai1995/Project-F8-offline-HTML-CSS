dcd140f5720053a08f092764df2c30a5b375d989
phanvanhai <phanvanhaixdddk49@gmail.com>
2024-02-28 11:45:38 +0700

    near finish

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,44 +1,15 @@
-// hide hidden dropdown
+// hide navbar
 
-const popUp = document.querySelector(".navbar-popup");
-const dropDown = document.querySelector(".dropdown");
-const circle = document.querySelector(".dropdown-circle");
+const navbar = document.querySelector(".navbar");
+const listIcon = document.querySelector(".header-icon__list");
+const overlay = document.querySelector(".header-overlay");
 
-// Use onclick
-// let pop = 0;
-// popUp.addEventListener("click", function () {
-//   ++pop;
-//   if (pop % 2 === 0) {
-//     document.querySelector(".dropdown").classList.add("dropdown__hide");
-//   } else {
-//     document.querySelector(".dropdown").classList.remove("dropdown__hide");
-//   }
-// });
+listIcon.addEventListener("click", function () {
+  navbar.classList.add("navbar-hide");
+  overlay.style.display = "block";
+});
 
-// Use hover === onmouseover
-
-popUp.onmouseover = function () {
-  dropDown.classList.add("dropdown__hide");
-};
-
-dropDown.onmouseover = function () {
-  dropDown.classList.add("dropdown__hide");
-};
-
-circle.onclick = function () {
-  dropDown.classList.remove("dropdown__hide");
-};
-
-document.querySelector(".dropdown-form").onsubmit = function (event) {
-  dropDown.classList.remove("dropdown__hide");
-  event.preventDefault();
-  document
-    .querySelector(".dropdown-success")
-    .classList.add("dropdown-success__hide");
-};
-
-document.querySelector(".dropdown-success__circle").onclick = function () {
-  document
-    .querySelector(".dropdown-success")
-    .classList.remove("dropdown-success__hide");
-};
+overlay.addEventListener("click", function () {
+  navbar.classList.remove("navbar-hide");
+  overlay.style.display = "none";
+});
